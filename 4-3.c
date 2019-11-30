afe280ffd15668bc492083e8430b818d03846c3d
guokeyu-0112 <2897868864@qq.com>
2019-11-30 04:03:37 -0800

    added a lab4 4-3.c file

--- 4-3.c
+++ 4-3.c
@@ -1,50 +1,57 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
 #define MAXOP 100
 #define NUMBER '0'
-int gitop(char[]);
+
+int getop(char []);
 void push(double);
 double pop(void);
-main()
+
+int main()
 {
     int type;
     double op2;
     char s[MAXOP];
-    while ((type=gitop(s))!=EOF){
-        swith (type) {
-        case NUMBER:
-                push(atof(s));
-                break;
-        case '+':
-            push(pop() + pop());
+
+    while((type=getop(s))!=EOF){
+        switch(type){
+            case NUMBER:
+            push(atof(s));
             break;
-        case '*':
-            push(pop() * pop());
+        case'+':
+            push(pop()+pop());
             break;
-        case '-':
+        case'*':
+            push(pop()*pop());
+            break;
+        case'-':
             op2=pop();
-            push(pop() - op2);
+            push(pop()-op2);
             break;
-        case '/':
+        case'/':
             op2=pop();
-            if(op2!=0.0)
+            if(op2!=0.0){
                 push(pop()/op2);
-            else/* code */
+            }
+            else{
                 printf("error:zero divisor\n");
+            }
             break;
-        case '%':    
+        case'%':
             op2=pop();
-            if(op2!=0.0)
-                pish(fmod(pop(),op2));
-            else
-                printf("error: zero divisor\n");
+            if(op2!=0.0){
+                push(fmod(pop(),op2));
+            }
+            else{
+                printf("error:zero divisor\n");
+            }
             break;
-        case '\n':
+        case'\n':
             printf("\t%.8g\n",pop());
             break;
         default:
-            printf("error: unknown command %s\n",s);
+            printf("error:unknown command %s\n",s);
             break;
         }
     }
