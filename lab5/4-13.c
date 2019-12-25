dcd21fd5fab822fc3e32ea4ced277b27e12dd434
guokeyu-0112 <2897868864@qq.com>
2019-12-25 06:40:21 -0800

    added a 4-13.c file

--- lab5/4-13.c
+++ lab5/4-13.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
-#include <string.h>
-char* reverse(char *x)
+#define MAX 101
+void reverse(int *input,int num)
 {
-    int len=strlen(x);
-    int i;
-    char c;
-    for(i=0;i<=len/2;i++)
-    {
-        c=x[i];
-        x[i]=x[len-i-1];
-        x[len-i-1]=c;
+    if(num>1){
+        int c=*(input+num-1);
+        *(input+num-1)=*input;
+        *input=c;
+        reverse(input+1,num-2);
     }
-    return x;
 }
 int main()
 {
-    char s[100];
-    printf("input a string:");
-    gets(s);
-    puts(reverse(s));
-    return 0;
+    int input[MAX];
+    for(int i=0;i<MAX;i++){
+        input[i]='\0';
+    }
+    int len=0;
+    int c=getchar();
+    while(c!='\n'&&c!=EOF)
+    {
+        input[len]=c;
+        len++;
+        if(len>=MAX){
+            printf("the input is too long");
+            return 0;
+        }
+        c=getchar();
+    }
+    reverse(input,len);
+    printf("the inverted sentence is: \n");
+    for(int i=0;i<len;i++)
+    {
+        printf("%c",input[i]);
+    }
+    printf("\n");
 } 
