8fdee7ff1c53dbd603db6d89517c58a45bd97fd0
guokeyu-0112 <2897868864@qq.com>
2019-12-26 03:08:27 -0800

    lab6

--- lab6/5-10.c
+++ lab6/5-10.c
@@ -1,62 +1,44 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <math.h>
+#include <ctype.h>
 #define MAXOP 100
 #define NUMBER '0'
-int getop(char[]);
-void ungets(char[]);
+int getop(char s[]);
 void push(double);
 double pop(void);
-int main(int argc,char *argv[])
+char s[MAXOP];
+int main(int argc, char *argv[])
 {
-    double op1;
+    int type;
     double op2;
-    char s[MAXOP];
-
-    while(--argc>0){
-        ungets("");
-        ungets(*++argv);
-        switch(getop(s)){
-            case NUMBER:
-            push(atof(s));
+    int j = 1;
+    int a = argc;
+    while((argc--)>0 && j<a){
+    type = getop(argv[j++]);
+        switch (type)
+        {
+        case NUMBER:
+            push(atof(argv[j-1]));
             break;
-            case'+':
-            push(pop()+pop());
+        case '+':
+            push(pop() + pop());
             break;
-            case'*':
-            push(pop()*pop());
+        case '*':
+            push(pop() * pop());
             break;
-            case'-':
-            op2=pop();
-            push(pop()-op2);
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
             break;
-            case'%':
-            op1=pop();
-            op2=pop();
-            if(op1!=(int)op1||op2!=(int)op2)
-                printf("error\n");
-            else if(op1==0)
-                printf("error:zero divisor\n");
-            else if(op2<0)
-                push((int)op2%(int)op1);
-            else if(op1<0)
-                push((int)op2%(int)op1);
-            else
-                push((int)op2%(int)op1);
-            break;
-            case'/':
-            op2=pop();
-            if(op2!=0.0)
-                push(pop()/op2);
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
             else
                 printf("error:zero divisor\n");
             break;
-            case '\n':
-            printf("\t%.8g\n",pop());
-            break;
-            default:
-            printf("error:unknown command %s\n",s);
+        default:
+            printf("error:unknown command %s\n", s);
             argc=1;
             break;
         }
@@ -65,59 +47,34 @@ int main(int argc,char *argv[])
     return 0;
 }
 #define MAXVAL 100
-int sp=0;
+int sp = 0;
 double val[MAXVAL];
-void push(double f){
-    if(sp<MAXVAL)
-    val[sp++]=f;
+void push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
     else
-        printf("error:stack full,can't push %g\n",f);
+    {
+        printf("error:stack full,can't push %g\n", f);
+    }
 }
-    double pop(void)
+double pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else
     {
-        if(sp>0)
-            return val[--sp];
-        else{
-            printf("error:stack empty\n");
-            return 0.0;
-        }
+        printf("error:stack empty\n");
+        return 0.0;
     }
-    int getch(void);
-    void ungetch(int);
-    int getop(char s[])
-    {
-        int i,c;
-        while((s[0]=c=getch())==' '||c=='\t')
-            ;
-        s[1]='\0';
-        if(!isdigit(c)&&c!='.')
-            return c;
-        i=0;
-        if(isdigit(c))
-            while(isdigit(s[++i]=c=getch()))
-                ;
-        if(c=='.')
-            while(isdigit(s[++i]=c=getch()))
-                ;
-        s[i]='\0';
-        if(c!=EOF)
-            ungets(c);
+}
+
+int getop(char s[])
+{
+    if (*s> '9' || *s < '0')
+        return *s;
+    else
         return NUMBER;
-    }
-    #define BUFSIZE 100
-    char buf[BUFSIZE];
-    int bufp=0;
-    int getch(void)
-    {
-        return(bufp>0)?buf[--bufp]:getchar();
-    }
-    void ungetch(int c)
-    {
-        if(bufp>=BUFSIZE)
-            printf("ungetch:too many characters\n");
-        else
-        {     
-             buf[bufp++]=c; 
-        } 
-    }
+        
+}
 
