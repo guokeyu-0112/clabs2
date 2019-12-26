fb1128450b68af384d02890530f8e2b940d2a8aa
guokeyu-0112 <2897868864@qq.com>
2019-12-26 04:00:32 -0800

    lab6

--- lab6/timediff.c
+++ lab6/timediff.c
@@ -1,37 +1,35 @@
-#include <stdio.h>
-int sum(int year,int month,int day)
+#include<stdio.h>
+#include<stdlib.h>
+static char daytab[2][13]={
+    {0,31,28,31,30,31,30,31,31,30,31,30,31},
+    {0,31,29,31,30,31,30,31,31,30,31,30,31}
+};
+int day_of_year(int year,int month,int day)
 {
-    unsigned char x[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-    int i,s=0;
-    for(i=1;i<year;i++)
-    if((i%4==0&&i%100!=0)||i%400==0)
-    s+=366;
-    else
-    {
-        s+=365;
-    }
-    if((year%4==0&&year%100!=0)||year%400==0)
-    x[2]=29;
+    int i,leap;
+    leap =((year%4 == 0)&&(year%100 !=0))||(year%400 == 0);
     for(i=1;i<month;i++)
-    s+=x[i];
-    s+=day;
-    return s;   
+    day+=daytab[leap][i];
+    return day;
 }
-int main()
+void month_day(int year,int yearday,int *pmonth,int *pday)
 {
-    unsigned char year1,month1,day1,year2,month2,day2;
-    int s1,s2;
-    printf("please enter the first date:");
-    scanf("%hhd%hhd%hhd",&year1,&month1,&day1);
-    printf("please enter the second date:");
-    scanf("%hhd%hhd%hhd",&year2,&month2,&day2);
-    s1=sum(year1,month1,day1);
-    s2=sum(year2,month2,day2);
-    if(s1>s2)
-    printf("%d\n",s1-s2+1);
-    else
-    {
-        printf("%d\n",s2-s1+1);
-    }
-    
+    int i,leap;
+    leap =((year%4 == 0)&&(year%100 != 0))||(year%400 ==0);
+    for(i = 1;yearday>daytab[leap][i];i++)
+    yearday -= daytab[leap][i];
+    *pmonth = i;
+    *pday = yearday;
+}
+int main(int argc,char *argv[])
+{
+    int day=0;
+    for(int i = atof(argv[1])+1;i<atof(argv[4]);i++){
+        day += day_of_year(i,12,31);}
+    if(atof(argv[1]) < atof(argv[4]))
+        day += day_of_year(atof(argv[4]),atof(argv[5]),atof(argv[6])) + day_of_year(atof(argv[1]),12,31) - day_of_year(atof(argv[1]),atof(argv[2]),atof(argv[3])) + 1;
+    else if(atof(argv[1]) == atof(argv[4]))
+        day += day_of_year(atof(argv[4]),atof(argv[5]),atof(argv[6])) - day_of_year(atof(argv[1]),atof(argv[2]),atof(argv[3])) + 1;
+    printf("%d\n",day);
+    return 0;
 }
